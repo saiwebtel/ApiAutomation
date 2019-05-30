b27e92d8a23db43d1288bbb2cad4a1d1c27181f1
Vinay Singh <Vinay.Singh@rsystems.com>
2019-05-30 12:22:58 +0530

    Validations

--- src/test/java/base/Testbase.java
+++ src/test/java/base/Testbase.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.testng.annotations.AfterSuite;
@@ -20,10 +21,10 @@ import java.util.Random;
 public class Testbase 
 {
 	public static Properties Repository = new Properties();
-	public int RESPONSE_STATUS_CODE_200=200;
-	public int RESPONSE_STATUS_CODE_400=400;
-	public int RESPONSE_STATUS_CODE_500=500;
-	public Connection con;
+	public static int RESPONSE_STATUS_CODE_200=200;
+	public static int RESPONSE_STATUS_CODE_400=400;
+	public static int RESPONSE_STATUS_CODE_500=500;
+	public Connection con,con1;
 	public String URL_FOR_COMIC="jdbc:oracle:thin:@10.67.200.35:1521:ADELEDB";
 	public String USERNAME_FOR_COMIC="SVT_COMICS_29NOV17";
 	public String PASSWORD_FOR_COMIC="SVT_COMICS_29NOV17";
@@ -32,16 +33,17 @@ public class Testbase
 	public String PASSWORD_FOR_UUSD="SVTUUSD_MIG_17MAR17";
 	public File file;
 	public FileInputStream fileinputstream;
-	public ResultSet resultset;
-	public Statement statement;
+	public ResultSet resultset,resultset1;
+	public Statement statement,statement1;
 	public String tvid;
 	public static int i=0;
+	public int subscriberNA=0;
     //Testbase testbase=new Testbase();
 	Random rand = new Random();
 	HttpRequestHandler httprequesthandler;
 
 	
-	@BeforeSuite
+	@BeforeSuite(enabled=true)
 	public void initialiseDB()
 	{
 		getConnectionForComicDatabse();
@@ -52,7 +54,8 @@ public class Testbase
 	@AfterSuite
 	public void closeConnection() throws SQLException
 	{
-		//con.close();
+		con.close();
+		con1.close();
 	}
 	public  void loadPropertiesFile() throws IOException
 	{
@@ -67,7 +70,8 @@ public class Testbase
 		    Class.forName("oracle.jdbc.driver.OracleDriver");  
 			//step2 create  the connection object  
 			con=DriverManager.getConnection(URL_FOR_COMIC,USERNAME_FOR_COMIC,PASSWORD_FOR_COMIC); 
-			//con=DriverManager.getConnection(URL_FOR_UUSD,USERNAME_FOR_UUSD,PASSWORD_FOR_UUSD); 
+			System.out.println("Comic connection Established");
+			System.out.println(con);
              }
          catch(Exception e)
 		    { 
@@ -80,8 +84,9 @@ public class Testbase
 			//step1 load the driver class  
 		    Class.forName("oracle.jdbc.driver.OracleDriver");  
 			//step2 create  the connection object  
-			con=DriverManager.getConnection(URL_FOR_UUSD,USERNAME_FOR_UUSD,PASSWORD_FOR_UUSD); 
-			System.out.println("Established");
+			con1=DriverManager.getConnection(URL_FOR_UUSD,USERNAME_FOR_UUSD,PASSWORD_FOR_UUSD); 
+			System.out.println("UUSD connection Established");
+			System.out.println(con1);
              }
          catch(Exception e)
 		    { 
@@ -90,38 +95,57 @@ public class Testbase
 	}
 	public String getTvidFromUUSD() throws SQLException
 	{
-		statement=con.createStatement();
-		resultset=statement.executeQuery("select SUBSCRIBER_NA from CUSTOMER_NA where STATUS_CODE='A' and CNA_UUID between 1 and 2000");
+		initialiseDB();
+		statement1=con1.createStatement();
+		resultset1=statement1.executeQuery("select SUBSCRIBER_NA from CUSTOMER_NA where STATUS_CODE='A' and CNA_UUID between 1 and 3000");
 		ArrayList<String> arraylist=new ArrayList<String>();
-
-		while(resultset.next())  
+		while(resultset1.next())  
 		{
-			arraylist.add(resultset.getString(1));
+			arraylist.add(resultset1.getString(1));
 		}
-		//System.out.println(arraylist.size());
-		String tvid=arraylist.get(rand.nextInt(100) + 1);
-		//System.out.println(tvid);
+		tvid=arraylist.get(rand.nextInt(100) + 1);
 		return tvid;
 	}
-	public void getDataFromComic() throws SQLException
-	{
-		statement=con.createStatement();
-		resultset=statement.executeQuery("select * from CONSENTS");
-		ArrayList<String> arraylist=new ArrayList<String>();
 
+	public List<String> getDataFromComicdatabase() throws SQLException
+	{
+		ArrayList<String> arraylist=new ArrayList<String>();
+		try{
+		System.out.println("=========>"+tvid);
+		statement=con.createStatement();
+		resultset=statement.executeQuery("select * from CONSENTS where TVID="+tvid);
+		System.out.println("=========>"+tvid);
 		while(resultset.next())  
 		{
-			arraylist.add(resultset.getString(2)+""+resultset.getString(3)+""+resultset.getString(4)+""+resultset.getString(5)+""+resultset.getString(6)+""+resultset.getString(7)+""+resultset.getString(8)+""+resultset.getString(9)+""+resultset.getString(10)+""+resultset.getString(11));
-		}
+			arraylist.add(resultset.getString(3));
+			arraylist.add(resultset.getString(4));
+			arraylist.add(resultset.getString(5));
+			arraylist.add(resultset.getString(7));
+		    arraylist.add(resultset.getString(8));
+		   System.out.println("---->"+arraylist.get(0));
+		} 
+		int size = arraylist.size(); 
+		System.out.println("size of ArrayList after clearing elements: " + size);
 
-	}
+		
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		 return arraylist;		
+		}
+	
+
+
 	public static void main(String[] args) throws SQLException 
 	{
-	   
-		Testbase ts=new Testbase();
-		ts.getConnectionForUUSDDatabse();
-		//ts.getConnectionForComicDatabse();
-		ts.getTvidFromUUSD();
+			Testbase ts=new Testbase();
+			//ts.getConnectionForUUSDDatabse();
+	  	    //ts.getTvidFromUUSD();
+			//ts.getDataFromComic();
+	  	    //ts.getDataFromComicdatabase();
+
 	}
 	
 }
