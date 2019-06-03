44517e10dbcf9fcd78663cff8fb001060a3e6ca2
Vinay Singh <Vinay.Singh@rsystems.com>
2019-06-03 13:46:33 +0530

    Added one more test case for CRS

--- src/test/java/test/testCusService.java
+++ src/test/java/test/testCusService.java
@@ -10,6 +10,7 @@ import model.Consents;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPut;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -29,8 +30,9 @@ public class testCusService
 	    public static int i=0;
 	    HttpClient httpclient;
 	    HttpResponse httpresponse;
-	    String FinalURL;
+	    String FinalURL,FinalURLForCRS;
 	    HttpPut httpput;
+	    HttpGet httpget;
 	    String stvid;
 	    List<String> dataFromComic;
 
@@ -42,8 +44,10 @@ public class testCusService
 		String ip=Testbase.Repository.getProperty("comic_ip");
 		String port=Testbase.Repository.getProperty("comic_port");
 		String base=Testbase.Repository.getProperty("BASE_FOR_COMIC");
+		String base1=Testbase.Repository.getProperty("BASE_FOR_COMIC_GET");
 		stvid=testbase.getTvidFromUUSD();
 		FinalURL="http://" + ip +":"+ port + base + stvid ;
+		FinalURLForCRS="http://" + ip +":"+ port + base1 + stvid ;
 	}
 
 	@DataProvider()
@@ -89,6 +93,60 @@ public class testCusService
 		Assert.assertEquals("ACT",dataFromComic.get(2));
 		Assert.assertEquals("Automation",dataFromComic.get(3));
 		Assert.assertEquals("Script",dataFromComic.get(4));
+	}
+	@DataProvider()
+	public Object[][] sendDataForCusAPI_TVReco_optin()
+	{
+		Object[][] obj=new Object[1][4];
+		obj[0][0]="TVReco";
+		obj[0][1]="OPTIN";
+		obj[0][2]="Automation";
+		obj[0][3]="Script";
+		
+		return obj;
+		
+	}
+	@Test(dataProvider="sendDataForCusAPI_TVReco_optin",description="This Test Verifies the CUS call with TVReco-OPTIN")
+	public void VerifyCUS_With_TVReco_Optin(String consentType,String consentValue,String lastupdatedby,String tvconsentmessageid) throws JsonGenerationException, JsonMappingException, IOException, SQLException
+	{
+		Consents consents = new Consents();
+		consents.setConsentType(consentType);
+		consents.setConsentValue(consentValue);
+		consents.setLastUpdatedBy(lastupdatedby);
+		consents.setConsentMessage(tvconsentmessageid);
+		
+		httpput=new HttpPut();
+		//httpput.addHeader("Content-Type","application/json");
+		ObjectMapper objectmapper=new ObjectMapper();
+		//object to json file
+		objectmapper.writeValue(new File("C:/Users/vinay.singh.IND-DEL/APIAUTOMATION_PROJECT/ApiAutomation/src/resources/consents.json"),consents);
+		
+		String userJsonstring=objectmapper.writeValueAsString(consents);
+		/*System.out.println(userJsonstring);
+		System.out.println(FinalURL);*/
+		httpresponse=httprequesthandler.SendRequest(FinalURL,userJsonstring);
+	  
+		int statusCode=httpresponse.getStatusLine().getStatusCode();
+		Assert.assertEquals(testbase.RESPONSE_STATUS_CODE_200, statusCode);
+		//Reporter.log("Data stored in Comic Database for TVID"+stvid);
+		dataFromComic=testbase.getDataFromComicdatabase();
+		System.out.println("Data stored in Comic Database for TVID===> "+stvid);
+		System.out.println("Data stored in Comic Database for TVID===> "+dataFromComic);
+		Assert.assertEquals("TVReco",dataFromComic.get(0));
+		Assert.assertEquals("OPTIN",dataFromComic.get(1));
+		Assert.assertEquals("ACT",dataFromComic.get(2));
+		Assert.assertEquals("Automation",dataFromComic.get(3));
+		Assert.assertEquals("Script",dataFromComic.get(4));
+	}
+	// CRS API TEST
+	@Test(description="This Test Verifies the CRS API")
+	public void VerifyCRSAPI() throws JsonGenerationException, JsonMappingException, IOException, SQLException
+	{
+		httpget=new HttpGet();
+		httpresponse=httprequesthandler1.SendGetRequest(FinalURLForCRS);
+		System.out.println("FINAL URL :::::::: "+FinalURLForCRS);
+		int statusCode=httpresponse.getStatusLine().getStatusCode();
+		Assert.assertEquals(testbase.RESPONSE_STATUS_CODE_200, statusCode);
 	}
 	
 public static void main(String args[]) throws SQLException, IOException
